15f77e54440e106ce988fa749517ca0542d262db
Luiz Felipe <luizfelipems12@gmail.com>
2023-09-05 12:36:42 -0300

    Initial Commit

---
+++ Livraria.Domain/Commands/Livro/Input/ApagarLivroCommand.cs
@@ -0,0 +1,28 @@
+﻿using Flunt.Notifications;
+using Livraria.Domain.Interfaces.Commands;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Livraria.Domain.Commands.Livro.Input
+{
+    public class ApagarLivroCommand : Notifiable, ICommandPadrao
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+
+        public bool ValidarCommand()
+        {
+            try
+            {
+                if (Id == Guid.Empty)
+                    AddNotification("Id", "Id é um campo obrigatório");
+
+                return Valid;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
